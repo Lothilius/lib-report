306586c478f11df61a72b670d3d648c471129847
Lothilius <martin@oi915.com>
2014-11-24 15:13:32 -0600

    Include file to pass uploaded file from php to the python file that changes the format to csv

---
+++ run_python.php
@@ -0,0 +1,22 @@
+<?php
+    date_default_timezone_set('America/Chicago');
+
+
+    $file = $_FILES['uploadedfile']['tmp_name'];
+    $names = $_FILES['uploadedfile']['name'];
+
+    $target = $dir.'In/'.$names;
+    move_uploaded_file( $_FILES['uploadedfile']['tmp_name'], $target);
+
+    $command = escapeshellcmd("python read.py $target");
+    $output = shell_exec($command);
+    echo "<pre>";
+    print_r($output);
+    echo "</pre>";
+
+//  Re-route to directory to download the file.
+    $domain = $_SERVER['HTTP_HOST'];
+    $path = implode("/",$fullPath);
+    $toLogin = 'http://'.$domain.$path."/out";
+    header('Location:'.$toLogin);
+?>
